8d16788c30870be9883f253da4a6a7d205d6e8f7
BK <bramkuijk@gmail.com>
2018-03-22 11:21:16 +0100

    Compilable script, function unchanged

--- TTN.ino
+++ TTN.ino
@@ -1,7 +1,7 @@
 #include <TheThingsNetwork.h>
 #include <DHT.h>
 
-//ttn keys
+//--ttn keys
 const char *devAddr = "26011834";
 const char *nwkSKey = "68812A2DF5C6E54EF18E197B5136C9DB";
 const char *appSKey = "F2F66085A51D09A5A86E74A9AF9B8D68";
@@ -12,10 +12,10 @@ const char *appSKey = "F2F66085A51D09A5A86E74A9AF9B8D68";
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
-//Pin defines
+//--Pin defines
 const int buttonPin = 8;
 
-//Global variables
+//--Global variables
 int buttonState = 0;
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -24,17 +24,17 @@ TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
 void setup()
 {
 
-  //serial setup
+  //--serial setup
   loraSerial.begin(57600);
   debugSerial.begin(9600);
-  debugSetup()
+  debugSetup();
 
   //ttn setup
   ttn.personalize(devAddr, nwkSKey, appSKey);
   ttn.showStatus();
   dht.begin();
 
-  //PinMode setup
+  //--PinMode setup
   pinMode(buttonPin, INPUT);
 }
 
@@ -42,57 +42,53 @@ void loop()
 {
   //debugSerial.println("-- LOOP");
 
-  buttonState = digitalRead(buttonPin);
+  int buttonState = digitalRead(buttonPin);
 
-  // Read sensor values and multiply by 100 to effictively have 2 decimals
-  // No decimal value available for the humidity model we're using.
+  //-- Read sensor values and multiply by 100 to effictively have 2 decimals
+  //-- No decimal value available for the humidity model we're using.
   uint16_t humidity = (dht.readHumidity(false)) * 100;
 
-  // false: Celsius (default)
-  // true: Farenheit
+  //-- false: Celsius (default)
+  //-- true: Farenheit
   uint16_t temperature = (dht.readTemperature(false)) * 100;
 
-  // Split both words (16 bits) into 2 bytes of 8
-  payload = buildPayload(temperature, humidity,button);
-
-  //Print values to serial monitor, only for debugging
-  debugPrint();
-  
-  //send payload
-  ttn.sendBytes(payload, sizeof(payload));
+  //--generate payload and send it
+  sendPayload(temperature, humidity, buttonState);
+  //--Print values to serial monitor, only for debugging
+  //debugPrint(temperature, humidity, buttonState);
 
   delay(10000);
 }
 
-// construction of the payload, takes all the values and constructs 
-// one array of bytes. Returns the array. 
-void buildPayload(int temp, int hum, int button)
+void sendPayload(int temp, uint16_t hum, int button)
 {
-	byte payload[5];
-  	payload[0] = highByte(temp);
-  	payload[1] = lowByte(temp);
-  	payload[2] = highByte(hum);
-  	payload[3] = lowByte(hum);
-  	payload[4] = lowByte(button);
+  byte payload[5];
+  payload[0] = highByte(temp);
+    payload[1] = lowByte(temp);
+    payload[2] = highByte(hum);
+    payload[3] = lowByte(hum);
+    payload[4] = lowByte(button);
 
-  	return payload
+    ttn.sendBytes(payload, sizeof(payload));
 }
 
-//Wait 10 seconds for the serial to initialize. 
-//Send info
+
+//--Wait 10 seconds for the serial to initialize. 
+//--Send info to serial monitor
 void debugSetup()
 {
-	while (!debugSerial && millis() < 10000);
-  	debugSerial.println("-- PERSONALIZE");
-  	debugSerial.println("-- STATUS");
+  while (!debugSerial && millis() < 10000);
+    debugSerial.println("-- PERSONALIZE");
+    debugSerial.println("-- STATUS");
 }
 
-void debugPrint()
+//--Print the temperature humidity and button state to the serial monitor
+void debugPrint(uint16_t temperature, uint16_t humidity, int buttonState)
 {
-	debugSerial.print("Temperature: ");
-  	debugSerial.println(temperature);
-  	debugSerial.print("Humidity: ");
-  	debugSerial.println(humidity);
-  	debugSerial.print("ButtonState: ");
-  	debugSerial.println(buttonState);
+  debugSerial.print("Temperature: ");
+    debugSerial.println(temperature);
+    debugSerial.print("Humidity: ");
+    debugSerial.println(humidity);
+    debugSerial.print("ButtonState: ");
+    debugSerial.println(buttonState);
 }
